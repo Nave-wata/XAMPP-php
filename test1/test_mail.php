07ca1ea2cf9d95c87a04443a814ac7a60fd2fd42
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-04-01 15:26:54 +0900

    添付ファイル付きのメール送信を実装

--- test1/test_mail.php
+++ test1/test_mail.php
@@ -9,17 +9,47 @@
     mb_language("Japanese");
     mb_internal_encoding("UTF-8");
 
-    $to = $_POST['to'];
-    $title = $_POST['title'];
-    $message = $_POST['message'];
-    $headers = "From: towa7261@gmail.com";
+    //宛先
+    $to = htmlspecialchars($_POST['to']);
+    //送信者
+    $from = "hoge21119@gmail.com";
+    //件名
+    $subject = "テスト送信です";
+    //本文
+    $text = "テスト送信です";
 
-    if (mb_send_mail($to, $title, $message, $headers)) {
-        echo "メール送信成功です";
-    } else {
-        echo "メール送信失敗です";
-    }
-    ?>
+    //添付したいファイルのパス
+    $file_path = "./test.txt";
+    //ファイル名
+    $file = "test.txt";
+
+    //ヘッダー設定
+    $header = "Content-Type: multipart/mixed;boundary=\"__BOUNDARY__\"\n";
+    $header .= "Return-Path: " . $to . " \n";
+    $header .= "From: " . $from ." \n";
+    $header .= "Sender: " . $from ." \n";
+    $header .= "Reply-To: " . $to . " \n";
+
+    //本文設定
+    $body = "--__BOUNDARY__\n";
+    $body .= "Content-Type: text/plain; charset=\"ISO-2022-JP\"\n\n";
+    $body .= $text . "\n";
+    $body .= "--__BOUNDARY__\n";
+
+    //添付ファイル設定
+    $body .= "Content-Type: application/octet-stream; name=\"{$file}\"\n";
+    $body .= "Content-Disposition: attachment; filename=\"{$file}\"\n";
+    $body .= "Content-Transfer-Encoding: base64\n";
+    $body .= "\n";
+    $body .= chunk_split(base64_encode(file_get_contents($file_path)));
+    $body .= "--__BOUNDARY__\n";
+
+        if (mb_send_mail( $to, $subject, $body, $header)) {
+            echo "メール送信成功です";
+        } else {
+            echo "メール送信失敗です";
+        }
+        ?>
 </body>
 
 </html>
